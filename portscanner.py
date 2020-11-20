a414f0e05a073c318abeb9928f6cf22571640a27
Paul Paradis <pauldparadis@protonmail.com>
2020-11-19 21:35:22 -0500

    Added files.
    Finished portscanner.

--- portscanner.py
+++ portscanner.py
@@ -4,19 +4,38 @@ from IPy import IP
 #establish connection with target machine
 #see if port is open
 #socket library allows us to establish a connection over the internet
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n' + '[- 0 Scanning Target]' + str(target))
+    for port in range(1, 100):
+        scan_port(converted_ip, port)
 
-port = 80
+def check_ip(ip):
+    try:
+        Ip(ip)
+        return(ip)
+    except Valueerror:
+        return socket.gethostbyname(ip)
 
-def scan port(ipaddress, port):
+def get_banner(s):
+    return s.recv(1024)
+
+def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
         sock.settimeout(0.5)
         sock.connect((ipaddress. port))
-        print('[+] Port '+ str(port) +' is open.')
+        try:
+            banner = get_banner(sock)
+            print('[+] Open Port ' + str(port) + ' : ' + str(banner.decode().strip('\n')))
+        except:
+            print('[+] Open Port ' + str(port))
     except:
-        print('[+] Port '+ str(port) +' is closed.')
+        pass
 
-ipaddres = input('[+] Enter target to scan: ')
-
-for port in range(1, 10):
-    scan_port(ipaddress, port)
+targets = input('[+] Enter target/s to scan (split multiple targets with ,)')
+if ',' in targets:
+    for ip_add in targets.split(','):
+        scan(ip_add.strip(' '))
+else:
+    scan(targets)
